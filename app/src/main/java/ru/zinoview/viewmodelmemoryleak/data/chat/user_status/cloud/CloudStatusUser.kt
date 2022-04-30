9f1262e14752147ebf34a4e40e60e4520908cbef
kostazinovev <k.gig@list.ru>
2022-04-30 15:43:54 +0300

    remove unused files

--- app/src/main/java/ru/zinoview/viewmodelmemoryleak/data/chat/user_status/cloud/CloudStatusUser.kt
+++
@@ -1,8 +0,0 @@
-package ru.zinoview.viewmodelmemoryleak.data.chat.user_status.cloud
-
-interface CloudStatusUser {
-
-    class Base(
-        private val notificationToken: String
-    ) : CloudStatusUser
-}
